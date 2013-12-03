0b28f8fd2b680987863af874e0dd3b4ea002f24f
Abby <amose679@gmail.com>
2013-12-03 14:02:38 -0500

    MORE
    just fucked with JQuery

--- learningJQuery.js
+++ learningJQuery.js
@@ -1,34 +1,57 @@
-// $( document ).ready(function() {
+$( document ).ready(function() {
 //   $( "button" ).click(function() {
 //   confirm('here');
 //   $( this ).remove();
 // });
-// });
+
+});
 
 //<button class=listButton> X </button>
 
-function addKeyword(){
-  var x=document.forms["myForm"]["fname"].value;
-  //confirm('here');
-  var y = '<li class=i>'+x+' </li>';
-  //var y = '<li>'+x+'</li>';
+function test(){
+	confirm('here');
+	var z = document.getElementById("special").remove();
+	(z).remove();
+}
+
+function addKeyword(n){
+  var x=document.forms["myForm"][n].value;
+  var y = '<li class=i onclick=rf(this)>'+x+' </li>';
+  var z = document.getElementById("special").remove();
+  (z).remove();
+  $('#list').append(y);
+  y='<li class="i" id="special" onclick="special(this)">+</li>' ;
+  $('#list').append(y); 
+
+}
+function specialAddKeyword(n, toDelete){
+  var x=document.forms["myForm"][n].value;
+  var y = '<li class=i onclick=rf(this)>'+x+' </li>';
+  $('#list').append(y);
+  (toDelete).remove();
+  y='<li class="i" id="special" onclick="special(this)">+</li>' ;
   $('#list').append(y);
 }
-function deleteKeyword(){
-  // var x=document.forms["myForm"]["fname"].value;
-  // //confirm('here');
-  // var y = '<li class=i>'+x+' <img src=closeButton.jpg id=im> onclick=deleteKeyword()</li>';
-  // //var y = '<li>'+x+'</li>';
-  this.getElementID;
-  //confirm("workin?");
-}
-function rf(){
-	confirm('here');
-	//confirm(z);
-	$('li', this).remove();
-	//$(a).remove();
+
+
+function deleteKeyword(e){
+	(e).remove();
+	return false;
 }
 
-//confirm('here');
+function special(e){
+	//prompt('yeah?');
+	(e).remove();
+	var y = '<input type="text" name="specialInputName" id="specialInput" autofocus onchange="specialAddKeyword(name, this)">' ;
+	$('#list').append(y);
 
+	//$('#list').append(y);
+}
+
+function rf(e){
+	//alert(e.id);
+	//alert('yo');
+	(e).remove();
+	return false;
+}
 
